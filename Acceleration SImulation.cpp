071d0030766a1c9b9f0633e9c8d1d53410bb4159
Michael Stroh <64543833+Michael-Stroh@users.noreply.github.com>
2020-10-23 14:16:25 +0200

    Add files via upload

---
+++ Acceleration SImulation.cpp
@@ -0,0 +1,11 @@
+#include "Acceleration SImulation.h"
+
+Acceleration_SImulation::Acceleration_SImulation() {
+	// TODO - implement Acceleration SImulation::Acceleration SImulation
+	throw "Not yet implemented";
+}
+
+Simulation* Acceleration_SImulation::simulate(CarComposite* car, Driver* drivre) {
+	// TODO - implement Acceleration SImulation::simulate
+	throw "Not yet implemented";
+}
