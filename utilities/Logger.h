e9fa563e3af3f045a08534514bc7937da111aaec
Alex Human (u19069716) <u19069716@tuks.co.za>
2020-10-24 15:39:19 +0200

    Logger Class Doxygen Comments
    Added all the necessary doxygen comments for the Logger class.

--- utilities/Logger.h
+++ utilities/Logger.h
@@ -1,3 +1,11 @@
+/**
+   @file Logger.h
+   @class Logger
+   @authors Alex
+   @version 1.0.0
+   @brief A class to provide logging functionality. Static methods are used to allow for logging in 6 different colours allowed by UNIX terminals: Red, Green, Yellow, Blue, Magenta & Cyan.
+ */
+
 #ifndef LOGGER_H
 #define LOGGER_H
 
@@ -7,12 +15,42 @@ using namespace std;
 class Logger
 {
 public:
+    /**
+     @brief Prints a specified heading & message to the console.
+     @details All other functions in the Logger class function in the same manner.
+     @param[in] heading: Heading of the logged message
+     @param[in] message: The message body to be logged
+    */
     static void log(string, string);
+
+    /**
+     @brief Prints a specified heading & message to the console in red
+    */
     static void red(string, string);
+
+    /**
+     @brief Prints a specified heading & message to the console in green
+    */
     static void green(string, string);
+
+    /**
+     @brief Prints a specified heading & message to the console in yellow
+    */
     static void yellow(string, string);
+
+    /**
+     @brief Prints a specified heading & message to the console in blue
+    */
     static void blue(string, string);
+
+    /**
+     @brief Prints a specified heading & message to the console in magenta
+    */
     static void magenta(string, string);
+
+    /**
+     @brief Prints a specified heading & message to the console in cyan
+    */
     static void cyan(string, string);
 };
 
