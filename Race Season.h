a5a6254079f2b57119f4fca714477ff6a3756cc7
Alex Human (u19069716) <u19069716@tuks.co.za>
2020-10-24 16:06:26 +0200

    RaceSeason Doxygen Comments
    Started on adding all of the doxygen comments for the RaceSeason class.
    I've completed comments for the constructor that accepts arguments & the
    runSeason() method.

--- Race Season.h
+++ Race Season.h
@@ -1,9 +1,9 @@
 /**
    @file Race_Season.h
    @class Race_Season
-   @authors Michael
+   @authors Alex
    @version 1.0.0
-   @brief 
+   @brief Responsible for running all of the Grand Prixs in a season and ensuring all participating teams are aware of the season before it starts as well keeping them updated on its progress while it takes place.
  */
 
 #ifndef RACE SEASON_H
@@ -14,62 +14,61 @@
 #include "Result.h"
 #include "Subject.h"
 
-class Race_Season {
+class Race_Season
+{
 
-	public:
-	
-		/**
+public:
+	/**
 			Constructor
 		*/
-		Race_Season();
+	Race_Season();
 
-		/**
+	/**
 			Constructor
-			@param
-			@param
-			@param
-			@param
+			@param[in] g: All the Grand_Prix that will take place this race season.
+			@param[in] t: All the Racing_Team that will be participating in the race season.
+			@param[in] r: An instantiated (but not yet containing any information) RaceSeasonResult that will be added to as the RaceSeason unfolds.
+			@param[in] s: The SeasonSubject, used to notify RaceTeams of the particulars of the Race_Season before or during the season.
 		*/
-		Race_Season( GrandPrix*, RacingTeam*, Result*, Subject* );
+	Race_Season(GrandPrix *, RacingTeam *, Result *, Subject *);
 
-		/**
+	/**
 			Destructor
 		*/
-		~Race_Season();
-	
-		/**
-			
-			@return
-		*/
-		Result* runSeason();
+	~Race_Season();
 
-		/**
+	/**
+			@brief Runs the entire race season
+			@details Will iterate through the collection of GrandPrixs, running each individually calling the GrandPrix.runGrandPrix() method. This will return a result for each GrandPrix, which is added to the SeasonResult (thus aggregating each GrandPrixs result into a final RaceSeasonResult).
+			@return A RaceSeasonResult object, that can be used to access the results of the entire RaceSeason.
+		*/
+	Result *runSeason();
+
+	/**
 			
 		*/
-		void prepareSeason();
-		
-	private:
-	
-		/**
+	void prepareSeason();
+
+private:
+	/**
 			@brief 
 		*/
-		GrandPrix* grandPrixs;
-		
-		/**
+	GrandPrix *grandPrixs;
+
+	/**
 			@brief 
 		*/
-		RacingTeam* teams;
-		
-		/**
+	RacingTeam *teams;
+
+	/**
 			@brief 
 		*/
-		Result* result;
-		
-		/**
+	Result *result;
+
+	/**
 			@brief 
 		*/
-		Subject* seasonSubject;
-	
+	Subject *seasonSubject;
 };
 
 #endif
