28582ee78895ce2c2f950974e557944343d4be37
Alex Human (u19069716) <u19069716@tuks.co.za>
2020-10-28 22:36:09 +0200

    PracticeState Implementation
    Implemented the following functions:
    - Constructor
    - Destructor
    - runRace()

--- PracticeState.cpp
+++ PracticeState.cpp
@@ -2,18 +2,50 @@
 
 PracticeState::PracticeState() : RaceState()
 {
-
-	// TODO - implement PracticeState::PracticeState
-	throw "Not yet implemented";
 }
 
 PracticeState::~PracticeState()
 {
 }
 
-Result *PracticeState::runRace(Result *result, RaceTeam *teams, Circuit *circuit)
+Result *PracticeState::runRace(Result *result, vector<RaceTeam *> *teams, Circuit *circuit)
 {
+	// 3 practice sessions
+	// #1 & #2 are 1.5 hours long, #3 is 1 hour long
 
-	// TODO - implement PracticeState::runRace
-	throw "Not yet implemented";
+	// General algorithm:
+	// Have all teams perform a single lap, store the longest laptime & subtract this from a variable representing the time left. Loop until there's no time left
+
+	Result *raceResult = new RaceResult();
+	float timeleft;
+	float longestLapTime;
+
+	// Races #1, #2 & #3:
+	for (int i = 0; i < 3; i++)
+	{
+		if (i == 2)
+		{
+			timeleft = 3600.00; // 1 hour stored as seconds
+		}
+		else
+		{
+			timeleft = 5400.00; // 1.5 hours stored as seconds
+		}
+		longestLapTime = 0;
+		// @todo: account for if last lap puts timeleft into the negative
+		while (timeleft > 0)
+		{
+			for (vector<RaceTeam *>::iterator team = teams->begin(); team != teams->end(); ++team)
+			{
+				// @todo: account for multiple drivers per team
+				LapResult *lap = team->performLap();
+				raceResult->addResult(lap);
+				if (lap->getLapTime() > longestLapTime)
+				{
+					longestLapTime = lap->getLapTime();
+				}
+			}
+			timeleft -= longestLapTime;
+		}
+	}
 }
