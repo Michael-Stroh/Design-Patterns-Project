c6a68d93c150f7d05823436da32fc10d97fe1ee5
Michael Stroh <64543833+Michael-Stroh@users.noreply.github.com>
2020-11-03 15:50:47 +0200

    Spacing

--- RaceTrack.cpp
+++ RaceTrack.cpp
@@ -1,29 +1,24 @@
 #include "RaceTrack.h"
 
-RaceTrack::RaceTrack() : Circuit()
-{
+RaceTrack::RaceTrack(): Circuit() {
+
 }
 
-RaceTrack::RaceTrack(string name) : Circuit(name)
-{
+RaceTrack::RaceTrack( string name ): Circuit( name ) {
 }
 
-RaceTrack::RaceTrack(string name, float dist, float wind) : Circuit(name), distance(dist), windForce(wind)
-{
+RaceTrack::RaceTrack( string name, float dist, float wind ): Circuit( name ), distance( dist ), windForce( wind ) {
 }
 
-RaceTrack::RaceTrack(string name, RaceTrack::direction dir, float dist, float wind, float strDist,
-					 int corners, int RaceLaps) : Circuit(name), direct(dir), distance(dist), windForce(wind),
-												  laps(RaceLaps), numCorners(corners), straightDistance(strDist)
-{
+RaceTrack::RaceTrack( string name, RaceTrack::direction dir, float dist, float wind, float strDist,
+					 int corners, int RaceLaps ): Circuit( name ), direct( dir ), distance( dist ), windForce( wind ),
+												  laps( RaceLaps ), numCorners( corners ), straightDistance( strDist ) {
 }
 
-RaceTrack::~RaceTrack()
-{
+RaceTrack::~RaceTrack() {
 }
 
-void RaceTrack::print()
-{
+void RaceTrack::print() {
 
 	//convert the floats to a string
 	stringstream dist, wind;
@@ -31,143 +26,122 @@ void RaceTrack::print()
 	dist << distance;
 	wind << windForce;
 	//output the details of the road/track
-	Logger::cyan("Circuit raced", getName() + " circuit was raced " + getDirection() + " with a distance of " + dist.str() + "km and a wind force of " + wind.str() + " units.");
+	Logger::cyan( "Circuit raced", getName() + " circuit was raced " + getDirection() + " with a distance of " + dist.str() + "km and a wind force of " + wind.str() + " units." );
 }
 
-Iterator *RaceTrack::createIterator()
-{
+Iterator *RaceTrack::createIterator() {
 
 	//create and return an iterator on the current object
 	//return ( new CircuitIterator( this ) );
 	return nullptr;
 }
 
-float RaceTrack::getDistance() const
-{
+float RaceTrack::getDistance() const {
 
 	return distance;
 }
 
-void RaceTrack::setDistance(float dist)
-{
+void RaceTrack::setDistance( float dist ) {
 
 	distance = dist;
 }
 
-float RaceTrack::getWindForce() const
-{
+float RaceTrack::getWindForce() const {
 
 	return windForce;
 }
 
-void RaceTrack::setWindForce(float wind)
-{
+void RaceTrack::setWindForce( float wind ) {
 
 	windForce = wind;
 }
 
-int RaceTrack::getLaps() const
-{
+int RaceTrack::getLaps() const {
 
 	return laps;
 }
 
-void RaceTrack::setLaps(int RaceLaps)
-{
+void RaceTrack::setLaps( int RaceLaps ) {
 
 	laps = RaceLaps;
 }
 
-int RaceTrack::getCorners() const
-{
+int RaceTrack::getCorners() const {
 
 	return numCorners;
 }
 
-void RaceTrack::setCorners(int corners)
-{
+void RaceTrack::setCorners( int corners ) {
 
 	numCorners = corners;
 }
 
-float RaceTrack::getStraightDistance() const
-{
+float RaceTrack::getStraightDistance() const {
 
 	return straightDistance;
 }
 
-void RaceTrack::setStraightDistance(float dist)
-{
+void RaceTrack::setStraightDistance( float dist ) {
 
 	straightDistance = dist;
 }
 
-string RaceTrack::getDirection() const
-{
+string RaceTrack::getDirection() const {
 
 	//determine which direction the car is going
-	switch (direct)
-	{
+	switch ( direct ) {
 
-	case clockwise:
-		return "clockwise";
-	case anticlockwise:
-		return "anti-clockwise";
-	default:
-		return "Error";
+		case clockwise:
+			return "clockwise";
+		case anticlockwise:
+			return "anti-clockwise";
+		default:
+			return "Error";
 	}
 }
 
-void RaceTrack::setDirection(RaceTrack::direction dir)
-{
+void RaceTrack::setDirection( RaceTrack::direction dir ) {
 
 	direct = dir;
 }
 
-bool RaceTrack::getEuro() const
-{
+bool RaceTrack::getEuro() const {
 
 	return isEuropean;
 }
 
-void RaceTrack::setEuro(bool euro)
-{
+void RaceTrack::setEuro( bool euro ) {
 
 	isEuropean = euro;
 }
 
-float RaceTrack::getAvgPitStops() const
-{
+float RaceTrack::getAvgPitStops() const {
 
 	return averagePitStop;
 }
 
-void RaceTrack::setAvgPitStops(float amount)
-{
+void RaceTrack::setAvgPitStops( float amount ) {
 
 	averagePitStop = amount;
 }
 
-string RaceTrack::getEndDate() const
-{
+string RaceTrack::getEndDate() const {
 
 	return endingDate;
 }
 
-void RaceTrack::setEndDate(string date)
-{
+void RaceTrack::setEndDate( string date ) {
 
 	endingDate = date;
 }
 
-string RaceTrack::getStartDate() const
-{
+string RaceTrack::getStartDate() const {
 
 	return startingDate;
 }
 
-void RaceTrack::setStartDate(string date)
-{
+void RaceTrack::setStartDate( string date ) {
 
 	startingDate = date;
 }
+
