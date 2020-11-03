80df5493410d12b786da6d83a31584feeb4491be
Michael Stroh <64543833+Michael-Stroh@users.noreply.github.com>
2020-11-03 15:30:41 +0200

    Pulled from Alex

--- GrandPrix.cpp
+++ GrandPrix.cpp
@@ -1,16 +1,60 @@
 #include "GrandPrix.h"
+#include "RaceResult.h"
 
-GrandPrix::GrandPrix() {
-	// TODO - implement Grand Prix::Grand Prix
-	throw "Not yet implemented";
+GrandPrix::GrandPrix()
+{
+	this->circuit = NULL;
+	this->race = new Race();
+	this->result = new GrandPrixResult();
 }
 
-Result* GrandPrix::runGrandPrix(RaceTeam* teams) {
-	// TODO - implement Grand Prix::runGrandPrix
-	throw "Not yet implemented";
+GrandPrix::GrandPrix(Circuit *c)
+{
+	this->circuit = c;
+	this->race = new Race();
+	this->result = new GrandPrixResult();
 }
 
-void GrandPrix::displayWinners() {
-	// TODO - implement Grand Prix::displayWinners
-	throw "Not yet implemented";
+GrandPrix::~GrandPrix()
+{
+	if (this->race)
+	{
+		delete this->race;
+	}
+	this->race = NULL;
+
+	if (this->result)
+	{
+		delete this->result;
+	}
+	this->result = NULL;
+}
+
+Result *GrandPrix::runGrandPrix(vector<RaceTeam *> *teams)
+{
+	// Perform the practice race
+	this->race->setState("Practice");
+	RaceResult *practiceResult = new RaceResult(this->race->runRace(NULL, teams, this->circuit));
+
+	// Perform the qualifying race
+	this->race->setState("Qualifying");
+	RaceResult *qualifyingResult = new RaceResult(this->race->runRace(NULL, teams, this->circuit));
+
+	// Perform the official race
+	this->race->setState("Official");
+	RaceResult *officialResult = new RaceResult(this->race->runRace(qualifyingResult, teams, this->circuit));
+
+	// Add official race's result to the grand prix's result, return the grand prix's result
+	this->result.addResult(officialResult);
+	return this->result;
+}
+
+void GrandPrix::displayResult()
+{
+	this->result->print();
+}
+
+void GrandPrix::setCircuit(Circuit *)
+{
+	this->circuit = c;
 }
