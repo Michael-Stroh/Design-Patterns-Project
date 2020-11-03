36b726ad926be803d2f76ecffe1ee6319ecb1d48
Alex Human (u19069716) <u19069716@tuks.co.za>
2020-11-03 09:41:35 +0200

    RaceTeam Subject Methods
    Implemented the following methods that interact with the Observer design
    pattern (methods called by the various subjects that RaceTeams will
    observe),
    
    - informSeasonResult()
    - updateQualifyingResult()
    - updateOfficialResult()
    - informGrandPrix()
    
    Also added very general operation intention for the performLap() method

--- RaceTeam.h
+++ RaceTeam.h
@@ -4,24 +4,32 @@
    @authors Michael
    @version 1.0.0
    @brief 
+   @todo: Implement constructor
+   @todo: Implement destructor
    @todo: Implement performLap(int, circuit) with int for driver index and circuit for driving on
    @todo: Implement updateSeasonResult()
    @todo: Implement informGrandPrixs()
    @todo: Implement updateQualifyingResult()
    @todo: Implement updateOfficialResult()
    @todo: Implement setRaceState()
+   @todo: Implement performLap()
  */
 
 #ifndef RACING_TEAM_H
 #define RACING_TEAM_H
 
 #include "Result.h"
+#include "RaceResult.h"
+#include "RaceSeasonResult.h"
 #include "GrandPrix.h"
 #include "Driver.h"
 #include "Car.h"
 #include "CarBuilder.h"
 #include "Strategies.h"
 #include "LapResult.h"
+#include "Circuit.h"
+#include <vector>
+using namespace std;
 
 class RaceTeam
 {
@@ -38,55 +46,81 @@ public:
 	~RaceTeam();
 
 	/**
-			
-			@param
-			@return
+	 		@brief: The method that will handle the team performing a lap on the circuit with a specific driver
+			@param[in]: int: The index of the driver to be performing the lap
+			@param[in]: Circuit*: The circuit on which the lap will take place
+			@return: The results of the lap
 		*/
-	LapResult *performLap(int);
+	LapResult *performLap(int, Circuit *);
 
 	/**
-			
-			@param
+			@brief: Sets the internal representation of the entire season's result
+			@details: Only needs to be notified once, the pointer is then updated accordingly
+			@param[in]: The current season's result
 		*/
-	void updateSeasonResult(int);
+	void informSeasonResult(Result *);
 
 	/**
-			
-			@param
+			@brief: Updates the internal representation of the qualifying race's result, so that the team can adjust its strategy accordingly
+			@param[in]: The current (qualifying) race's result
 		*/
-	void updateQualifyingRaceResult(int);
+	void updateQualifyingRaceResult(Result *);
 
 	/**
-			
-			@param
-			@return
+	 		@brief: Updates the internal representation of the official race's result, so that the team can adjust its strategy accordingly
+			@param[in]: The current (official) race's result
 		*/
-	bool updateOfficialRaceResult(Result *);
+	void updateOfficialRaceResult(Result *);
 
 	/**
-			
-			@param
+			@brief: Updates the internal representation of all the GrandPrixs, so that the team can prepare
+			@param[in]: A vector containing all GrandPrixs that will occur in a season
 		*/
-	void informGrandPrix(GrandPrix *);
+	void informGrandPrix(vector<GrandPrix *>);
 
 private:
 	/**
-			@brief
+			@brief: A vector containing all the grand prixs that will take place during a season
 		*/
-	Driver *driver;
+	vector<Driver *> grandPrixs;
 
 	/**
-			@brief
+			@brief: A vector containing the 2 drivers that drive per team
+		*/
+	vector<Driver *> drivers;
+
+	/**
+			@brief: The internal representation of the results of the current official race
+		*/
+	Result *officialRaceResult;
+
+	/**
+			@brief: The internal representation of the results of the current qualifying race
+		*/
+	Result *qualifyingRaceResult;
+
+	/**
+			@brief: The internal representation of the results of the current season
+		*/
+	Result *seasonResult;
+
+	/**
+			@brief: 
+			@todo: Provide details on car member variable
 		*/
 	Car *car;
 
 	/**
 			@brief
+			@todo: Provide details on the builder member variable
+
 		*/
 	CarBuilder *builder;
 
 	/**
 			@brief
+			@todo: Provide details on the strategy member variable
+
 		*/
 	Strategies *Strategy;
 };
