4718eaa7be33ee85cced1eab8dfa0324d74b3e60
Michael Stroh <64543833+Michael-Stroh@users.noreply.github.com>
2020-11-04 13:52:58 +0200

    roll back

--- CircuitIterator.cpp
+++ CircuitIterator.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "CircuitIterator.h"
 
 CircuitIterator::CircuitIterator() : Iterator(){
@@ -37,44 +36,4 @@ Circuit *CircuitIterator::currentItem() {
     */
 
     return array->getRoad( index );
-=======
-#include "CircuitIterator.h"
-
-CircuitIterator::CircuitIterator() : Iterator(){
-
-}
-
-CircuitIterator::CircuitIterator( CompositeRoad *track ) : Iterator(), array( track ) {
-
-}
-
-CircuitIterator::~CircuitIterator(){
-
-}
-
-void CircuitIterator::first() {
-
-    index = 0;
-}
-
-void CircuitIterator::next() {
-
-    index++;
-}
-
-bool CircuitIterator::isDone() {
-
-    //check if the current index is still in range
-    return ( array->getSize() > index );
-}
-
-Circuit *CircuitIterator::currentItem() {
-
-    /*
-        find and return the current object
-        this will return the leaf participant( RaceTrack object )
-    */
-
-    return array->getRoad( index );
->>>>>>> master
 }
