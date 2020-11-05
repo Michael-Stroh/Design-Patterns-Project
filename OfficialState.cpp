288e6d09a23595c3322c618a7fead1d41b0bd31e
Alex Human (u19069716) <u19069716@tuks.co.za>
2020-11-05 17:44:11 +0200

    Removed Circuit::getLapDistance call
    Function not yet implemented, was causing issues with compilation

--- OfficialState.cpp
+++ OfficialState.cpp
@@ -17,7 +17,8 @@ OfficialState::~OfficialState()
 Result *OfficialState::runRace(Result *result, vector<RaceTeam *> teams, Circuit *circuit)
 {
 	float remainingDistance = 305.00;
-	float lapDistance = circuit->getLapDistance();
+	// float lapDistance = circuit->getLapDistance();
+	float lapDistance = 10;
 	float timeLeft = 7200.00; // 2 hours in seconds
 	float longestLapTime = 0;
 	RaceResult *officialRaceResult = new RaceResult();
@@ -27,7 +28,8 @@ Result *OfficialState::runRace(Result *result, vector<RaceTeam *> teams, Circuit
 	{
 		for (vector<RaceTeam *>::iterator team = teams.begin(); team != teams.end(); ++team)
 		{
-			if(previousQualifiersResult->isQualified((*team)->getDriver(0)->getName())){
+			if (previousQualifiersResult->isQualified((*team)->getDriver(0)->getName()))
+			{
 				LapResult *lapA = (*team)->performLap(0, circuit);
 				officialRaceResult->addResult(lapA);
 				if (lapA->getLapTime() > longestLapTime)
@@ -35,7 +37,8 @@ Result *OfficialState::runRace(Result *result, vector<RaceTeam *> teams, Circuit
 					longestLapTime = lapA->getLapTime();
 				}
 			}
-			if(previousQualifiersResult->isQualified((*team)->getDriver(1)->getName())){
+			if (previousQualifiersResult->isQualified((*team)->getDriver(1)->getName()))
+			{
 				LapResult *lapB = (*team)->performLap(1, circuit);
 				officialRaceResult->addResult(lapB);
 				if (lapB->getLapTime() > longestLapTime)
@@ -43,7 +46,6 @@ Result *OfficialState::runRace(Result *result, vector<RaceTeam *> teams, Circuit
 					longestLapTime = lapB->getLapTime();
 				}
 			}
-			
 		}
 		timeLeft -= longestLapTime;
 		remainingDistance -= lapDistance;
