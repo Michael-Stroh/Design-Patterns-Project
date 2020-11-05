8f92badce1720ce2f42f7faf66edb99dc8ab0f02
Michael Stroh <64543833+Michael-Stroh@users.noreply.github.com>
2020-11-05 20:14:54 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -22,10 +22,81 @@ int main() {
 	raceSeason->printFinalResults();
 	//deletion
 	deleteEverything();
+
+
+
+
+
+
+
+	/*
+	                ******IDEA ALGORITHM******
+
+            //Creation
+            GrandPrix** gps;
+            RaceTeam** teams;
+            //RaceSeason * raceSeason = new RaceSeason( teams, gps );
+            RaceSeason* raceSeason = new RaceSeason();
+            //Notification
+            raceSeason->prepareSeason();
+            //RaceLoop
+            int numGrandPrixs = 0;
+            for ( int i = 0; i < numGrandPrixs; ++ i ) {
+                prepareForNextRace( teams, days ); //do Engineering
+                raceSeason->runNextRace();        //run race
+                printCurrentGrandprixResult();
+            }
+            raceSeason->printFinalResults();
+            //deletion
+            deleteEverything();
+	 */
 }
 void prepareForNextRace( team, days) {
 	for ( int i = 0; i < numTeams; ++ i ) {
 		teams[ i ]->prepareForNextRace(); //Brent strategy call once lekker
 	}
 */
+
+void prepareForNextRace( RaceTeam* team, string days ) {
+
+    /*
+    for ( int i = 0; i < numTeams; ++ i ) {
+
+        teams[ i ]->prepareForNextRace(); //Brent strategy call once lekker
+    }
+     */
+
+}
+
+
+void populateCircuit( const string& fileName ) {
+
+	ifstream file;
+
+	file.open( "../Data/races.txt" );
+	file.open( fileName );
+	if ( file.is_open() ) {
+
+		string line;
+		while ( getline( file, line ) ) {
+
+			int pos = 0;
+			int size = line.size();
+			cout << size << endl;
+			string temp = line;
+
+			for ( int i = 0; i < 10; i++ ) {
+
+				pos = temp.find_first_of( '|' );
+				cout << temp.substr( 1, pos - 1 ) << endl;
+				temp = temp.substr( pos + 1, size );
+			}
+		}
+	} else {
+		cout << "file not found" << endl;
+	}
+
+	file.close();
+
+
 }
